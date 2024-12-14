0b7f2d79948571f63df32fda91a62095f9e29270
hashemimohamadreza <hashemi.7677@gmail.com>
2024-12-14 20:44:28 +0330

    add user models in my app

--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
+
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    user_name=models.CharField(max_length=50,unique=True)
+    phone=models.CharField(max_length=50,unique=True)
+    profile_image=models.ImageField()
+    date_joined=models.DateTimeField(auto_now_add=True,null=True)
+    date_updated=models.DateTimeField(auto_now=True,null=True)
+
+    def __str__(self):
+        return self.email
+
+
